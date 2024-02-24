fb4f5880a358c3daa7b2a593e4c6bd91fa4e281e
Edoardo Di Genua <edoardo.dgn@gmail.com>
2024-02-24 21:07:35 +0100

    Fixed vimSetup.cmd

--- vimSetup.cmd
+++ vimSetup.cmd
@@ -5,7 +5,7 @@ set "DESTFOLDER=%LOCALAPPDATA%\nvim"
 if not exist "%DESTFOLDER%" (
   mkdir "%DESTFOLDER%
   echo Folder for neovim setup created, copy configuations files...
-  xcopy /s . "%DESTFOLDER%'
+  xcopy /s . "%DESTFOLDER%"
   del "%DESTFOLDER%\vimSetup.*"
   echo Setup complited
 ) else (
