c892e20b69d59105a1134b2e0ac8cb80418eef1f
Edoardo Di Genua <edoardo.dgn@gmail.com>
2024-02-25 09:45:26 +0100

    Switching to ESlint LSP from eslint_d none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
+				ensure_installed = { "lua_ls", "tsserver", "eslint" },
 			})
 		end,
 	},
@@ -18,6 +18,7 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
@@ -25,6 +26,10 @@ return {
 				capabilites = capabilities,
 			})
 
+			lspconfig.eslint.setup({
+				capabilites = capabilities,
+			})
+      
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
