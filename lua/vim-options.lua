c17f69dc058758a50e368371a6ee05987bf109e5
Edoardo Di Genua <edoardo.dgn@gmail.com>
2024-03-02 09:16:24 +0100

    Added autoclose for quickfix and styling for LSP hover

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,3 +10,10 @@ vim.cmd("set relativenumber")
 vim.cmd("set shada=!,'5,<50,s10,h")
 --Remap <C-c> to <Esc> or the LSP diagnostic will not work properly
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+--Close quickFix after selecting, really annoying
+vim.api.nvim_create_autocmd(
+  "FileType", {
+    pattern = { "qf" },
+    command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
+  })
