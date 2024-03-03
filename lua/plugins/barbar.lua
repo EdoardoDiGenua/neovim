6cbd034aae639b39afd604ffc282ff8af31ed19d
Edoardo Di Genua <edoardo.dgn@gmail.com>
2024-03-03 10:56:45 +0100

    Added max line lenght when formatting and added lsp_lines plugin

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,19 +1,19 @@
 return {
-	"romgrk/barbar.nvim",
-	dependencies = {
-		"lewis6991/gitsigns.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	init = function()
-		vim.keymap.set("n", "<C-b>", ":BufferPrevious<CR>", {})
-		vim.keymap.set("n", "<C-a>", ":BufferNext<CR>", {})
-		vim.keymap.set("n", "<Leader>bc", ":BufferClose<CR>", {})
-		vim.g.barbar_auto_setup = false
-	end,
-	opts = {
-		sidebar_filetypes = {
-			["neo-tree"] = { event = "BufWipeout" },
-		},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
-	},
+  "romgrk/barbar.nvim",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  init = function()
+    vim.keymap.set("n", "<C-b>", ":BufferPrevious<CR>", {})
+    vim.keymap.set("n", "<C-a>", ":BufferNext<CR>", {})
+    vim.keymap.set("n", "<Leader>bc", ":BufferClose<CR>", {})
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
+    sidebar_filetypes = {
+      ["neo-tree"] = { event = "BufWipeout" },
+    },
+    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+  },
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,99 +1,117 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"j-hui/fidget.nvim",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local mason_lspconfig = require("mason-lspconfig")
-			local lspconfig = require("lspconfig")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_lsp.default_capabilities()
-			)
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "j-hui/fidget.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local cmp_lsp = require("cmp_nvim_lsp")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities()
+      )
 
-			require("fidget").setup({})
-			require("mason").setup()
-			mason_lspconfig.setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
+      require("fidget").setup({})
+      require("mason").setup()
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim", "it", "describe", "before_each", "after_each" },
-						},
-					},
-				},
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            format = {
+              enable = true,
+              defaultConfig = {
+                max_line_length = "80",
+              },
+            },
+            diagnostics = {
+              globals = { "vim", "it", "describe", "before_each", "after_each" },
+            },
+          },
+        },
+      })
 
-			lspconfig.tsserver.setup({
-				capabilites = capabilities,
-				on_attach = function(client)
-					client.server_capabilities.documentFormattingProvider = false
-					client.server_capabilities.documentRangeFormattingProvider = false
-				end,
-			})
+      lspconfig.tsserver.setup({
+        capabilites = capabilities,
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        end,
+      })
 
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+        }, {
+          { name = "buffer" },
+        }),
+      })
 
-			vim.diagnostic.config({
-				--update_in_insert = true,
-				float = {
-					focusable = false,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.diagnostic.config({
+        --update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-				border = "single",
-			})
-		end,
-	},
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = "single",
+        })
+    end,
+  },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      vim.diagnostic.config({
+        virtual_text = false,
+      })
+      require("lsp_lines").setup()
+      vim.keymap.set("n", "<Leader>l", require("lsp_lines").toggle,
+        { desc = "Toggle lsp_lines" })
+    end,
+  },
 }
