db72eca571f3daf5912c4928f20dc7e558263cb4
Edoardo Di Genua <e.digenua@reply.it>
2024-10-03 12:36:09 +0200

    added more settings to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,33 +1,41 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"BurntSushi/ripgrep",
-			"sharkdp/fd",
-			"nvim-telescope/telescope-fzy-native.nvim",
-		},
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "BurntSushi/ripgrep",
+      "sharkdp/fd",
+      "nvim-telescope/telescope-fzy-native.nvim",
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
+    end,
+  },
 
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-			require("telescope").load_extension("fzy_native")
-		end,
-	},
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_config = {
+            horizontal = {
+              preview_cutoff = 0,
+            },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("fzy_native")
+    end,
+  },
 }
